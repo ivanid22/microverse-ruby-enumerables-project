ca0ae9e7ab1341e315fb904e97473b947d0fb4a1
Ivan Diaz <ivanid22@gmail.com>
2020-02-21 13:37:42 -0300

    Add my_each_with_index method

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-# rubocop:disable Style/CaseEquality
-
 module Enumerable
   def my_each
     pos = 0
@@ -10,10 +8,26 @@ module Enumerable
       pos += 1
     end
   end
+
+  def my_each_with_index
+    pos = 0
+    loop do
+      break if pos >= length
+
+      yield self[pos], pos
+      pos += 1
+    end
+  end
 end
 
 my_array = [1, 3, 5, 7]
 
+puts('my_each:')
 my_array.my_each do |element|
   puts element
+end
+
+puts("\nMy_each_with_index:")
+my_array.my_each_with_index do |element, index|
+  puts "Element at position #{index}: #{element}"
 end
