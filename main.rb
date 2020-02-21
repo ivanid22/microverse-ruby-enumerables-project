9cccd94b2ead181ce8c594618726b93b6a01b190
Ivan Diaz <ivanid22@gmail.com>
2020-02-21 14:15:20 -0300

    Add my_all method

--- main.rb
+++ main.rb
@@ -26,20 +26,35 @@ module Enumerable
     end
     return_arr
   end
+
+  def my_all
+    return_val = true
+    my_each do |element|
+      return_val = yield element
+    end
+    return_val
+  end
 end
 
-my_array = [1, 2, 3, 4, 5, 6, 7]
+my_integer_array = [1, 2, 3, 4, 5, 6, 7, 'n']
+my_mixed_array = [1, 2, 3, 'a', 'b', 'c']
 
 puts("\nmy_each:")
-my_array.my_each do |element|
+my_integer_array.my_each do |element|
   puts element
 end
 
 puts("\nmy_each_with_index:")
-my_array.my_each_with_index do |element, index|
+my_integer_array.my_each_with_index do |element, index|
   puts "Element at position #{index}: #{element}"
 end
 
 puts("\nmy_select (selecting even numbers):")
-evens = my_array.my_select(&:even?)
+evens = my_integer_array.my_select(&:even?)
 puts evens
+
+puts("\nmy_all (checking if all arguments are numbers): ")
+result = my_mixed_array.my_all do |element|
+  element.is_a? Integer
+end
+puts result
