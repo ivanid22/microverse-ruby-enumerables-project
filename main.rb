85eb6832eff243eb8383ad5bc19a2bc662965c83
Ivan Diaz <ivanid22@gmail.com>
2020-02-25 14:37:50 -0300

    Add output to show multiply_els is working

--- main.rb
+++ main.rb
@@ -115,3 +115,7 @@ end
 def multiply_els(arr)
   arr.my_inject(:*)
 end
+
+# The following lines are used for showcasing multiply_els, not for debugging purposes
+puts 'multiply_els [1, 2, 3, 4, 5]:'
+puts multiply_els([1, 2, 3, 4, 5])
